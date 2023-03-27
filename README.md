a9564a8b4bf44e5b3b67f01fcd42b360440648c6
zoobgod <zoomzoob@gmail.com>
2023-03-27 13:05:03 +0300

    week3 JS day1 added

--- README.md
+++ README.md
@@ -2,3 +2,5 @@
 The tasks lie in folders under the {week} + {week-number} tag.
 
 //** It was difficult for me to complete some of the tasks because they were very big and challenging. I enrolled in a part-time study program to spend less time on assignments like this. If I had completed these tasks, it would have been as if I were in a full-time program, which is not what I intended when I signed up for a part-time program. **//
+
+
---
+++ week3/script.js
@@ -0,0 +1,89 @@
+//let adressNumber = "2";
+//let adressStreet = "3";
+//let country = "Russia";
+//let globalAdress = "I live in" + "adressNumber" + "adressStreet" +  "country"
+
+//console.log(globalAdress)
+// console.log("I live in" + "adressNumber" + "adressStreet" + "country")
+
+
+
+//let txt = "ABCDEFGADVAVV";
+//let len = txt.length;
+//console.log(txt.length);
+
+//let str = "Please locate where 'locate' occurs!";
+//let pos= str.indexOf('locate');
+//console.log(pos);
+
+
+// let str = "Apple, Banana, Kiwi";
+// let banana = str.slice(7,13);
+// console.log(banana);
+
+
+// let str = "Please Visit Microsoft!";
+// console.log(str.toUpperCase());
+// let str1 = str.replace("Microsoft", "Developers Institute");
+// console.log(str1)
+
+// let txt1 = "Hello";
+// let txt2 = "World";
+// let txt3 = txt1 + ' ' + txt2;
+// let txt4 = txt1.concat('', txt2, 'banana');
+// console.log(txt4);
+
+// let str = "     Hello     ";
+// let str1 = str.rtrim();
+// console.log(str1);
+
+//Numbers Method
+
+// let a = 5;
+// a.toString()
+// console.log(a)
+
+// let x = 9.656;
+// x.toFixed(0) // 10
+// x.toFixed(2)  //9.66
+// x.toFixed(4) // 9.6560
+// x.toFixed(6) // 9.656000
+
+// let b = "1.5";
+// let c = Number(b);
+// parseInt();
+// parseFloat();
+// console.log(c);
+
+// isNaN(1);
+
+// let arr = ['Apple',['One', 'Two', 'Three'], 'Banana', 'Kiwi'];
+// let arr1 = new Array();
+// // console.log(arr);
+// // arr[0] ='Orange';
+// // console.log(typeOf(arr));
+// console.log(arr[1][1]);
+// let fruits = ['Banana', 'Kiwi'];
+// // let str = fruits.join('#');
+// // console.log(str);
+
+// // console.log(fruits.pop());
+// // console.log(fruits);
+// fruits.pop();
+// fruits.push('Mango');
+// console.log(fruits);
+
+
+// let pets = ['cat', 'Dog', 'Fish', 'rabbit', 'cow'];
+// console.log(pets[1]);
+
+// pets.push('Horse');
+// console.log(pets);
+
+// pets.splice(3,1);
+// console.log(pets);
+
+
+// // alert('mememememmeme');
+let name = prompt('What is your name?');
+console.log(name);
