a83183d003f10f12a33e7335aec5eb33f7658df4
zoobgod <zoomzoob@gmail.com>
2023-03-28 21:04:23 +0300

    updated to day 3 week3

---
+++ week3/day3/script.js
@@ -0,0 +1,130 @@
+// let b = 3, d = b, u = b;
+
+// const tree = ++d * d*b * b++ + //(4*3*3)*3=144+1
+//  + --d+ + +b-- + // (145 from the prev line)+4-1+4-1=
+// + +d*b+ + //+9
+//  u //+3
+
+//  console.log(tree)
+
+// let num =5;
+// if (num % 2 == 0 ) {
+//   console.log("Number is even")}
+
+//   else  {
+//     console.log("Number is odd")
+//   }
+
+
+// let a = 11;
+// let b = '19';
+// let c = 'abc';
+// let d = '21d';
+// let e = 21.5;
+
+// if (!(isNaN(a)) ) {
+//   console.log("Valid")
+
+// }
+
+// else {
+//   console.log("Not valid")
+// }
+
+// if (!(isNaN(b)) ) {
+//   console.log("Valid")
+
+// }
+
+// else {
+//   console.log("Not valid")
+// }
+
+// if (!(isNaN(c)) ) {
+//   console.log("Valid")
+
+// }
+
+// else {
+//   console.log("Not valid")
+// }
+
+// if (!(isNaN(d)) ) {
+//   console.log("Valid")
+
+// }
+
+// else {
+//   console.log("Not valid")
+// }
+
+
+// if (!(isNaN(e)) ) {
+//   console.log("Valid")
+
+// }
+
+// else {
+//   console.log("Not valid")
+// }
+
+
+// let num1 = 21;
+// let num2 = 45;
+// let num3 = 13;
+
+// let num1 = 34;
+// let num2 = 18;
+// let num3 = 52;
+
+
+// let num1 = 64;
+// let num2 = 11;
+// let num3 = 11;
+
+
+
+// if (num1>num2 && num1>num3) {
+// console.log("Num1 is the largest")
+// }
+// else if (num2>num1 && num2>num3) {
+//   console.log("Num2 is the largest")
+// }
+// else if (num3>num1 && num3>num2) {
+//   console.log("Num3 is the largest")
+// }
+
+
+
+
+// let age = (prompt())
+
+// if (age<18) {
+//   alert('nono u cant drive the VROOM VROOM')
+// }
+
+// else if (age ===18) {
+//   alert('wow cool can i have your number?')
+// }
+// else {
+//   alert('gratz! first car! very cool! very fast VROOM VROOM! have fun! ')
+// }
+
+// let user = {
+//   username: "jason",
+//   password: "FunLord1209",
+
+// }
+
+// let db = [];
+// db.push(user);
+
+// let newsfeed =  [
+//   {username:"Mikaela", timeline:1},
+//   {username:"Jony", timeline:2},
+//   {username:"Kolin", timeline:3}
+// ]
+
+// console.log(newsfeed[2])
+
+
